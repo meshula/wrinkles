04940f06fc48d13aa0ed85b7dc3d37572e560790
ssteinbach <ssteinbach@users.noreply.github.com>
2023-03-17 10:35:19 -0700

    building up tests and opentimelineio lib features

--- src/opentimelineio.zig
+++ src/opentimelineio.zig
@@ -15,6 +15,7 @@ const ALLOCATOR = allocator.ALLOCATOR;
 // just for roughing tests in
 pub const Clip = struct {
     name: ?string = null,
+    source_range: ?opentime.ContinuousTimeInterval = null,
 
     pub fn space(self: *Clip, label: string.latin_s8) !SpaceReference {
         return .{
@@ -22,10 +23,23 @@ pub const Clip = struct {
             .label= label,
         };
     }
+
+    pub fn topology(self: @This()) time_topology.TimeTopology {
+        if (self.source_range) |range| {
+            return time_topology.TimeTopology.init_identity(range);
+        } else {
+            return .{};
+        }
+    }
 };
 
 pub const Gap = struct {
     name: ?string = null,
+
+    pub fn topology(self: @This()) time_topology.TimeTopology {
+        _ = self;
+        return .{};
+    }
 };
 
 pub const Item = union(enum) {
@@ -38,6 +52,14 @@ pub const ItemPtr = union(enum) {
     clip_ptr: *Clip,
     gap_ptr: *Gap,
     track_ptr: *Track,
+
+    pub fn topology(self: @This()) time_topology.TimeTopology {
+        return switch (self) {
+            .clip_ptr => |cl| cl.topology(),
+            .gap_ptr => |gp| gp.topology(),
+            .track_ptr => |tr| tr.topology(),
+        };
+    }
 };
 
 pub const Track = struct {
@@ -55,6 +77,11 @@ pub const Track = struct {
             .label= label,
         };
     }
+
+    pub fn topology(self: @This()) time_topology.TimeTopology {
+        _  = self;
+        return .{};
+    }
 };
 
 const SpaceReference = struct {
@@ -69,10 +96,10 @@ const ProjectionOperatorArgs = struct {
 
 const ProjectionOperator = struct {
     args: ProjectionOperatorArgs,
-    crv: opentime.TimeTopology,
+    topology: opentime.TimeTopology,
 
     pub fn project_ordinate(self: @This(), ord_to_project: f32) !f32 {
-        return self.crv.project_seconds(ord_to_project);
+        return self.topology.project_seconds(ord_to_project);
     }
 };
 
@@ -80,11 +107,14 @@ pub fn build_projection_operator(
     args: ProjectionOperatorArgs
 ) !ProjectionOperator
 {
+    const source_topology = args.source.target.topology();
+    const destination_topology = args.destination.target.topology();
+
+    const source_to_dest_topo = source_topology.project_topology(destination_topology);
+
     return .{
         .args = args,
-        .crv = time_topology.TimeTopology.init_identity(
-            .{.start_seconds = 0, .end_seconds = 10}
-        )
+        .topology = source_to_dest_topo,
     };
 }
 
@@ -107,30 +137,30 @@ test "Track with clip with identity transform" {
     );
 }
 
-// test "Track with clip with identity transform and bounds" {
-//     var tr = Track {};
-//     var cl = Clip { .source_range = .{ .start_seconds = 0, .end_seconds = 2 } };
-//     try tr.append(.{ .clip = cl });
-//
-//     const track_to_clip = try build_projection_operator(
-//         .{
-//             .source = try tr.space("output"),
-//             .destination =  try cl.space("media")
-//         }
-//     );
-//
-//     try expectError(
-//         time_topology.TimeTopology.ProjectionError.OutOfBounds,
-//         track_to_clip.project_ordinate(3)
-//     );
-// }
+test "Track with clip with identity transform and bounds" {
+    var tr = Track {};
+    var cl = Clip { .source_range = .{ .start_seconds = 0, .end_seconds = 2 } };
+    try tr.append(.{ .clip = cl });
+
+    const track_to_clip = try build_projection_operator(
+        .{
+            .source = try tr.space("output"),
+            .destination =  try cl.space("media")
+        }
+    );
+
+    try expectError(
+        time_topology.TimeTopology.ProjectionError.OutOfBounds,
+        track_to_clip.project_ordinate(3)
+    );
+}
 
 // test "Single Clip With Transform" {
 //     // add an xform
-//     const crv = opentime.curve.read_curve_json(
+//     const topology = opentime.curve.read_curve_json(
 //         "curves/reverse_identity.curve.json"
 //     );
-//     var cl = Clip { .topology = crv };
+//     var cl = Clip { .topology = topology };
 //
 //     var tr = Track {};
 //     try tr.append(.{ .clip = cl });
@@ -140,7 +170,7 @@ test "Track with clip with identity transform" {
 //         try Clip.space("media")
 //     );
 //
-//     cl.topology_from_curve(crv);
+//     cl.topology_from_curve(topology);
 //
 //     try expectApproxEqAbs(@as(f32, 5), try track_to_clip.project_ordinate(3));
 // }
