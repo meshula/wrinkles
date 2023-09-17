f26f5038d019b5a6ff30864bc76ad2a9d42d0bac
ssteinbach <ssteinbach@users.noreply.github.com>
2023-09-17 11:12:17 -0700

    add assembly output to build.zig

--- experiments/comath_test/build.zig
+++ experiments/comath_test/build.zig
@@ -39,16 +39,47 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
 
     inline for (.{"comath_test.zig"}) |name| {
-        const unit_tests = b.addTest(.{
-            .root_source_file = .{ .path = name },
-            .target = target,
-            .optimize = optimize,
-        });
+        const unit_tests = b.addTest(
+            .{
+                .root_source_file = .{ .path = name },
+                .target = target,
+                .optimize = optimize,
+            }
+        );
+        const install_asm = b.addInstallFile(
+            unit_tests.getEmittedAsm(),
+            "file.asm"
+        );
+        b.getInstallStep().dependOn(&install_asm.step);
 
         unit_tests.addModule("comath", comath_dep.module("comath"));
 
         const run_unit_tests = b.addRunArtifact(unit_tests);
 
         test_step.dependOn(&run_unit_tests.step);
+        test_step.dependOn(&install_asm.step);
+    }
+
+    // Create executable for our example
+    const exe = b.addExecutable(.{
+        .name = "comath_test_app",
+        .root_source_file = .{ .path = "comath_test.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.addModule("comath", comath_dep.module("comath"));
+    // exe.emit_asm = .emit;
+
+    // Install the executable into the prefix when invoking "zig build"
+    b.installArtifact(exe);
+
+    const install_asm_on_build = false;
+    if (install_asm_on_build) {
+        const install_asm = b.addInstallFile(
+            exe.getEmittedAsm(),
+            "file.asm"
+        );
+        b.getInstallStep().dependOn(&install_asm.step);
     }
 }
