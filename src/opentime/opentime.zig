d3f956dcb8e482f5fb220b5c9e94b12e68e111a3
ssteinbach <ssteinbach@users.noreply.github.com>
2023-09-24 21:49:50 -0700

    better coordinates for the linear curve

--- src/opentime/opentime.zig
+++ src/opentime/opentime.zig
@@ -14,8 +14,12 @@ pub const Domain = @import("domain.zig").Domain;
 // transform
 pub const transform = @import("transform.zig");
 
+pub const dual = @import("dual.zig");
+pub const Dual_t = dual.Dual_t;
+
 test "all opentime tests" {
     _ = interval;
     _ = Domain;
     _ = transform;
+    _ = dual;
 }
