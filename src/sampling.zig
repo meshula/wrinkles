ac87482c7d77f6d9958fd60accf9060adfeff320
ssteinbach <ssteinbach@users.noreply.github.com>
2024-06-07 11:26:30 -0700

    sketch of rasterizing the sin and peak_to_peak_distance

--- src/sampling.zig
+++ src/sampling.zig
@@ -7,12 +7,101 @@ const expectEqual = std.testing.expectEqual;
 // @TODO : move to util maybe?
 const EPSILON: f32 = 1.0e-6;
 
+/// alias around the sample type @TODO: make this comptime definable (anytype)
+const sample_t  = f64;
+
 const SineSampleGenerator = struct {
     sampling_frequency_hz: u32,
     signal_frequency_hz: u32,
     signal_amplitude: f32,
     signal_duration_s: f32,
+
+    pub fn rasterized(
+        self:@This(),
+        allocator: std.mem.Allocator
+    ) ![]sample_t 
+    {
+        const sample_hz : sample_t = @floatFromInt(self.sampling_frequency_hz);
+
+        const result: []sample_t = try allocator.alloc(
+            sample_t,
+            @intFromFloat(@ceil(@as(f64, self.signal_duration_s)*sample_hz))
+        );
+
+        // fill the sample buffer
+        for (0.., result)
+            |idx, *sample|
+        {
+            sample.* = std.math.sin(
+                @as(sample_t, @floatFromInt(self.signal_frequency_hz))
+                * 2.0 * std.math.pi 
+                * (@as(sample_t, @floatFromInt(idx)) / sample_hz)
+            );
+        }
+
+        return result;
+    }
 };
+
+test "rasterizing the sine" {
+    const samples_48 = SineSampleGenerator{
+        .sampling_frequency_hz = 48000,
+        .signal_frequency_hz = 100,
+        .signal_amplitude = 1,
+        .signal_duration_s = 1,
+    };
+    const s48_buf = try samples_48.rasterized(std.testing.allocator);
+    defer std.testing.allocator.free(s48_buf);
+
+    // check some known quantities
+    try expectEqual(@as(usize, 48000), s48_buf.len);
+    try expectEqual(@as(sample_t, 0), s48_buf[0]);
+}
+
+// returns the peak to peak distance of the samples in the buffer
+pub fn peak_to_peak_distance(
+    samples: []const sample_t
+) usize 
+{
+    var last_max_idx:?usize = null;
+    var peak_to_peak_samples:usize = 0;
+
+    var idx : usize = 1;
+    while (idx < samples.len) 
+        : (idx += 1) 
+    {
+        if (
+            samples[idx] > samples[idx - 1] 
+            and samples[idx] > samples[idx + 1]
+        ) 
+        {
+            if (last_max_idx) 
+                |l_m_idx| 
+            {
+                peak_to_peak_samples = idx - l_m_idx;
+                break;
+            }
+            last_max_idx = idx;
+        }
+    }
+
+    return peak_to_peak_samples;
+}
+
+test "peak_to_peak_distance basic" {
+    const samples_48 = SineSampleGenerator{
+        .sampling_frequency_hz = 48000,
+        .signal_frequency_hz = 100,
+        .signal_amplitude = 1,
+        .signal_duration_s = 1,
+    };
+    const samples_48_buf = try samples_48.rasterized(std.testing.allocator);
+    defer std.testing.allocator.free(samples_48_buf);
+
+    const samples_48_p2p = peak_to_peak_distance(samples_48_buf);
+
+    try expectEqual(480, samples_48_p2p);
+}
 
 // test 0 - ensure that the contents of the c-library are visible
 test "c lib interface test" {
@@ -33,8 +122,10 @@ test "resample from 48khz to 44" {
         .signal_amplitude = 1,
         .signal_duration_s = 1,
     };
+    const samples_48_buf = try samples_48.rasterized(std.testing.allocator);
+    defer std.testing.allocator.free(samples_48_buf);
 
-    const samples_48_peak_to_peak_distance = samples_48.peak_to_peak_distance();
+    const samples_48_p2pd = peak_to_peak_distance(samples_48_buf);
 
     // find a zero crossing
     // const s_i_48_half = samples_48.sample_index_near_s(0.5);
@@ -57,14 +148,11 @@ test "resample from 48khz to 44" {
     // const s_48_half_next = samples_48.sample_at_index(s_i_48_half + 1);
 
     const samples_44 = samples_48.resampled(44100);
-    const samples_44_peak_to_peak_distance = samples_44.peak_to_peak_distance(
-        0.5
-    );
+    const samples_44_p2pd = samples_44.peak_to_peak_distance(0.5);
 
-    try expectEqual(
-        samples_48_peak_to_peak_distance,
-        samples_44_peak_to_peak_distance
-    );
+    // peak to peak distance (a distance in sample indices) should be the same
+    // independent of retiming those samples
+    try expectEqual(samples_48_p2pd, samples_44_p2pd);
 }
 
 // test 2
