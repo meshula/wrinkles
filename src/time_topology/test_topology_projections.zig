d2237d4b0731962c09664ea23d577bfaf83f45d6
ssteinbach <ssteinbach@users.noreply.github.com>
2024-06-19 21:59:21 -0700

    clean up some tests remove a skip

--- src/time_topology/test_topology_projections.zig
+++ src/time_topology/test_topology_projections.zig
@@ -8,7 +8,10 @@ const expectEqual = std.testing.expectEqual;
 const expectApproxEqAbs= std.testing.expectApproxEqAbs;
 const expectError = @import("std").testing.expectError;
 
-pub fn expectNotNan(val: f32) !void {
+pub fn expectNotNan(
+    val: f32
+) !void 
+{
     return try expect(std.math.isNan(val) == false);
 }
 
@@ -18,14 +21,17 @@ pub fn expectNotNan(val: f32) !void {
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-test "identity projections" {
+test "identity projections" 
+{
     const identity_inf = TimeTopology.init_identity_infinite();
 
     const bounds = opentime.ContinuousTimeInterval{
         .start_seconds = 12,
         .end_seconds = 20,
     };
-    const identity_bounded = TimeTopology.init_affine(.{ .bounds = bounds });
+    const identity_bounded = TimeTopology.init_affine(
+        .{ .bounds = bounds }
+    );
     try expectError(
         TimeTopology.ProjectionError.OutOfBounds, 
         identity_bounded.project_ordinate(10),
@@ -36,40 +42,67 @@ test "identity projections" {
         EPSILON
     );
 
-    const inf_through_bounded = try identity_bounded.project_topology(
-        std.testing.allocator,
-        identity_inf,
+    const inf_through_bounded = (
+        try identity_bounded.project_topology(
+            std.testing.allocator,
+            identity_inf,
+        )
     );
 
     // no segments because identity_inf has no segments
-
-    try expectEqual(inf_through_bounded.affine.bounds, bounds);
+    try expectEqual(
+        inf_through_bounded.affine.bounds,
+        bounds
+    );
 }
 
-test "projection test: linear_through_linear" {
+test "projection test: linear_through_linear" 
+{
     const first = TimeTopology.init_affine(
         .{ 
             .transform = .{ .scale = 4 }, 
-            .bounds = .{.start_seconds = 0, .end_seconds=30},
+            .bounds = .{
+                .start_seconds = 0,
+                .end_seconds=30
+            },
         }
     );
 
-    try expectEqual(@as(f32, 0), try first.project_ordinate(0));
-    try expectApproxEqAbs(@as(f32, 8), try first.project_ordinate(2), EPSILON);
+    try expectEqual(
+        @as(f32, 0),
+        try first.project_ordinate(0)
+    );
+    try expectApproxEqAbs(
+        @as(f32, 8),
+        try first.project_ordinate(2),
+        EPSILON
+    );
 
     const second = TimeTopology.init_affine(
         .{ 
             .transform = .{ .scale = 2},
-            .bounds = .{.start_seconds = 0, .end_seconds=15},
+            .bounds = .{
+                .start_seconds = 0,
+                .end_seconds=15
+            },
         }
     );
-    try expectEqual(@as(f32, 0), try second.project_ordinate(0));
-    try expectApproxEqAbs(@as(f32, 4), try second.project_ordinate(2), EPSILON);
+    try expectEqual(
+        @as(f32, 0),
+        try second.project_ordinate(0)
+    );
+    try expectApproxEqAbs(
+        @as(f32, 4),
+        try second.project_ordinate(2),
+        EPSILON
+    );
 
     // project one through the other
-    const second_through_first_topo = try first.project_topology(
-        std.testing.allocator,
-        second,
+    const second_through_first_topo = (
+        try first.project_topology(
+            std.testing.allocator,
+            second,
+        )
     );
     try expectEqual(
         @as(f32, 0),
@@ -82,49 +115,62 @@ test "projection test: linear_through_linear" {
     );
 }
 
-test "projection test: linear_through_linear with boundary" {
-    try util.skip_test();
+test "projection test: linear_through_linear with boundary" 
+{
+    // try util.skip_test();
 
     const first = TimeTopology.init_affine(
         .{ 
             .transform = .{ .scale = 4 },
-            .bounds = .{.start_seconds = 0, .end_seconds=5},
+            .bounds = .{
+                .start_seconds = 0,
+                .end_seconds=5
+            },
         }
     );
 
-    try expectEqual(@as(f32, 0), try first.project_ordinate(0));
-    try expectEqual(@as(f32, 8), try first.project_ordinate(2));
+    try expectEqual(0, try first.project_ordinate(0));
+    try expectEqual(8, try first.project_ordinate(2));
 
     // because intervals are right-open, projecting the boundary value is 
     // outside of the boundary condition
     try std.testing.expectError(
-        error.ProjectionError,
+        error.OutOfBounds,
         first.project_ordinate(5)
     );
 
     const second= TimeTopology.init_affine(
         .{ 
             .transform = .{ .scale = 2},
-            .bounds = .{.start_seconds = 0, .end_seconds=15},
+            .bounds = .{
+                .start_seconds = 0,
+                .end_seconds=15
+            },
         }
     );
-    try expectEqual(@as(f32, 0),  try second.project_ordinate(0));
-    try expectEqual(@as(f32, 4),  try second.project_ordinate(2));
+    try expectEqual(0,  try second.project_ordinate(0));
+    try expectEqual(4,  try second.project_ordinate(2));
 
     // not out of bounds for the second topology
-    try expectEqual(@as(f32, 10), try second.project_ordinate(5));
+    try expectEqual(10, try second.project_ordinate(5));
 
     // project one through the other
     const second_through_first_topo = try first.project_topology(
         std.testing.allocator,
         second
     );
-    try expectEqual(@as(f32, 0), try second_through_first_topo.project_ordinate(0));
-    try expectEqual(@as(f32, 16), try second_through_first_topo.project_ordinate(2));
+    try expectEqual(
+        @as(f32, 0),
+        try second_through_first_topo.project_ordinate(0)
+    );
+    try expectEqual(
+        @as(f32, 16),
+        try second_through_first_topo.project_ordinate(2)
+    );
 
     // ... but after projection, the bounds are smaller
     try std.testing.expectError(
-        error.ProjectionError,
+        error.OutOfBounds,
         first.project_ordinate(5)
     );
 }
