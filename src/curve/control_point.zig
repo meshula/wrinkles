fc5db4f3a79128ed3426e7c0d13a055d0fae9448
ssteinbach <ssteinbach@users.noreply.github.com>
2024-07-10 08:43:57 -0700

    cleanup in the control_point library

--- src/curve/control_point.zig
+++ src/curve/control_point.zig
@@ -1,3 +1,17 @@
+//! Control Point implementation
+//!
+//! Note that this module uses a pattern:
+//!   return switch (@typeInfo(@TypeOf(rhs))) {
+//!       .Struct => self.mul_cp(rhs),
+//!       else => self.mul_num(rhs),
+//!   };
+//!
+//! Other parts of the Wrinkles project use the "comath" library to do
+//! operator-overloaded math.  This pattern allows polymorphism with some type
+//! specification,in other words, mul can be called with either a float or a
+//! ControlPoint argument but not a []const u8, despite the anytype.
+//!
+
 const std = @import("std");
 const expectEqual = std.testing.expectEqual;
 const dual = @import("opentime").dual;
@@ -14,56 +28,88 @@ pub const ControlPoint = struct {
     value: f32 = 0,
 
     // polymorphic dispatch
-    pub fn mul(self: @This(), rhs: anytype) ControlPoint {
+    pub fn mul(
+        self: @This(),
+        rhs: anytype,
+    ) ControlPoint 
+    {
         return switch (@typeInfo(@TypeOf(rhs))) {
             .Struct => self.mul_cp(rhs),
             else => self.mul_num(rhs),
         };
     }
 
-    pub fn mul_num(self: @This(), val: f32) ControlPoint {
+    pub fn mul_num(
+        self: @This(),
+        val: f32,
+    ) ControlPoint 
+    {
         return .{
             .time = val*self.time,
             .value = val*self.value,
         };
     }
 
-    pub fn mul_cp(self: @This(), rhs: ControlPoint) ControlPoint {
+    pub fn mul_cp(
+        self: @This(),
+        rhs: ControlPoint,
+    ) ControlPoint 
+    {
         return .{
             .time = rhs.time*self.time,
             .value = rhs.value*self.value,
         };
     }
 
-    pub fn div(self: @This(), rhs: anytype) ControlPoint {
+    pub fn div(
+        self: @This(),
+        rhs: anytype,
+    ) ControlPoint 
+    {
         return switch (@typeInfo(@TypeOf(rhs))) {
             .Struct => self.div_cp(rhs),
             else => self.div_num(rhs),
         };
     }
 
-    pub fn div_num(self: @This(), val: f32) ControlPoint {
+    pub fn div_num(
+        self: @This(),
+        val: f32,
+    ) ControlPoint 
+    {
         return .{
             .time  = self.time/val,
             .value = self.value/val,
         };
     }
 
-    pub fn div_cp(self: @This(), val: ControlPoint) ControlPoint {
+    pub fn div_cp(
+        self: @This(),
+        val: ControlPoint,
+    ) ControlPoint 
+    {
         return .{
             .time  = self.time/val.time,
             .value = self.value/val.value,
         };
     }
 
-    pub fn add(self: @This(), rhs: anytype) ControlPoint {
+    pub fn add(
+        self: @This(),
+        rhs: anytype,
+    ) ControlPoint 
+    {
         return switch (@typeInfo(@TypeOf(rhs))) {
             .Struct => self.add_cp(rhs),
             else => self.add_num(rhs),
         };
     }
 
-    pub fn add_num(self: @This(), rhs: anytype) ControlPoint {
+    pub fn add_num(
+        self: @This(),
+        rhs: anytype,
+    ) ControlPoint 
+    {
         return .{
             .time = self.time + rhs,
             .value = self.value + rhs,
@@ -71,40 +117,63 @@ pub const ControlPoint = struct {
     }
 
 
-    pub fn add_cp(self: @This(), rhs: ControlPoint) ControlPoint {
+    pub fn add_cp(
+        self: @This(),
+        rhs: ControlPoint,
+    ) ControlPoint 
+    {
         return .{
             .time = self.time + rhs.time,
             .value = self.value + rhs.value,
         };
     }
 
-    pub fn sub(self: @This(), rhs: anytype) ControlPoint {
+    pub fn sub(
+        self: @This(),
+        rhs: anytype,
+    ) ControlPoint 
+    {
         return switch (@typeInfo(@TypeOf(rhs))) {
             .Struct => self.sub_cp(rhs),
             else => self.sub_num(rhs),
         };
     }
 
-    pub fn sub_cp(self: @This(), rhs: ControlPoint) ControlPoint {
+    pub fn sub_cp(
+        self: @This(),
+        rhs: ControlPoint,
+    ) ControlPoint 
+    {
         return .{
             .time = self.time - rhs.time,
             .value = self.value - rhs.value,
         };
     }
 
-    pub fn sub_num(self: @This(), rhs: anytype) ControlPoint {
+    pub fn sub_num(
+        self: @This(),
+        rhs: anytype,
+    ) ControlPoint 
+    {
         return .{
             .time = self.time - rhs,
             .value = self.value - rhs,
         };
     }
 
-    pub fn distance(self: @This(), rhs: ControlPoint) f32 {
+    pub fn distance(
+        self: @This(),
+        rhs: ControlPoint,
+    ) f32 
+    {
         const diff = rhs.sub(self);
         return std.math.sqrt(diff.time * diff.time + diff.value * diff.value);
     }
 
-    pub fn normalized(self: @This()) ControlPoint {
+    pub fn normalized(
+        self: @This(),
+    ) ControlPoint 
+    {
         const d = self.distance(.{ .time=0, .value=0 });
         return .{ .time = self.time/d, .value = self.value/d };
     }
@@ -141,7 +210,8 @@ pub fn expectControlPointEqual(
 }
 
 // @{ TESTS
-test "ControlPoint: add" { 
+test "ControlPoint: add" 
+{ 
     const cp1 = ControlPoint{ .time = 0, .value = 10 };
     const cp2 = ControlPoint{ .time = 20, .value = -10 };
 
@@ -150,7 +220,8 @@ test "ControlPoint: add" {
     try expectControlPointEqual(cp1.add(cp2), result);
 }
 
-test "ControlPoint: sub" { 
+test "ControlPoint: sub" 
+{ 
     const cp1 = ControlPoint{ .time = 0, .value = 10 };
     const cp2 = ControlPoint{ .time = 20, .value = -10 };
 
@@ -159,7 +230,8 @@ test "ControlPoint: sub" {
     try expectControlPointEqual(cp1.sub(cp2), result);
 }
 
-test "ControlPoint: mul" { 
+test "ControlPoint: mul" 
+{ 
     const cp1 = ControlPoint{ .time = 0.0, .value = 10.0 };
     const scale = -10.0;
 
