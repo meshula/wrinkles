f5b2e55b9e4fe2b7a6617daa652e0a0875739496
ssteinbach <ssteinbach@users.noreply.github.com>
2024-09-02 21:06:57 -0700

    add the bezier mapping

--- src/time_topology/mapping.zig
+++ src/time_topology/mapping.zig
@@ -22,7 +22,7 @@ pub const Mapping = union (enum) {
     empty: mapping_empty.MappingEmpty,
     affine: mapping_affine.MappingAffine,
     linear: mapping_curve_linear.MappingCurveLinear,
-    // bezier: MappingCurveBezier,
+    bezier: MappingCurveBezier,
 
     pub fn deinit(
         self: @This(),
@@ -35,8 +35,8 @@ pub const Mapping = union (enum) {
         };
     }
 
-    // project an instantaneous ordinate from the input space to the output
-    // space
+    /// project an instantaneous ordinate from the input space to the output
+    /// space
     pub fn project_instantaneous_cc(
         self: @This(),
         ord: Ordinate,
@@ -82,9 +82,97 @@ pub const Mapping = union (enum) {
     // @}
 };
 
+const curve = @import("curve");
 
 /// a Cubic Bezier Mapping from input to output
 pub const MappingCurveBezier = struct {
+    input_to_output_curve: curve.TimeCurve,
+
+    pub fn init_curve(
+        allocator: std.mem.Allocator,
+        crv: curve.TimeCurve,
+    ) MappingCurveBezier
+    {
+        return .{
+            .input_to_output_curve = try crv.clone(allocator),
+        };
+    }
+
+    pub fn init_segments(
+        allocator: std.mem.Allocator,
+        segments: []const curve.Segment,
+    ) !MappingCurveBezier
+    {
+        return .{
+            .input_to_output_curve = try curve.TimeCurve.init(
+                allocator,
+                segments,
+            )
+        };
+    }
+
+    pub fn deinit(
+        self: @This(),
+        allocator: std.mem.Allocator,
+    ) void 
+    {
+        self.input_to_output_curve.deinit(allocator);
+    }
+
+    /// project an instantaneous ordinate from the input space to the output
+    /// space
+    pub fn project_instantaneous_cc(
+        self: @This(),
+        ord: Ordinate,
+    ) !Ordinate 
+    {
+        return try self.input_to_output_curve.evaluate(ord);
+    }
+
+    /// fetch (computing if necessary) the input bounds of the mapping
+    pub fn input_bounds(
+        self: @This(),
+    ) opentime.ContinuousTimeInterval
+    {
+        return self.input_to_output_curve.extents_time();
+    }
+
+    /// fetch (computing if necessary) the output bounds of the mapping
+    pub fn output_bounds(
+        self: @This(),
+    ) opentime.ContinuousTimeInterval
+    {
+        return self.input_to_output_curve.extents_value();
+    }
+
+    pub fn mapping(
+        self: @This(),
+    ) Mapping
+    {
+        return .{
+            .bezier = self,
+        };
+    }
 };
 
+test "MappingCurveBezier: init and project"
+{
+    const mcb = (
+        try MappingCurveBezier.init_segments(
+            std.testing.allocator, 
+            &.{ 
+                curve.Segment.init_from_start_end(
+                    .{ .time = 0, .value = 0 },
+                    .{ .time = 10, .value = 20 },
+                ),
+            },
+            )
+    ).mapping();
+    defer mcb.deinit(std.testing.allocator);
+
+    try std.testing.expectEqual(
+        10,  
+        mcb.project_instantaneous_cc(5),
+    );
+}
 
