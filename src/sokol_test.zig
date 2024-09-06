b070398437c1c79c3a3c818800eeb927701f7b1d
ssteinbach <ssteinbach@users.noreply.github.com>
2024-09-06 12:30:48 -0700

    checkpointing sokol test

--- src/sokol_test.zig
+++ src/sokol_test.zig
@@ -72,33 +72,116 @@ fn drawGui() !void {
     // zgui.setNextWindowSize(.{ .w = width, .h = height, });
     //
 
-    c.igSetNextWindowPos(
-        .{ .x = 0, .y = 0 },
-        0,
-        .{},
-    );
 
-    var main_flags : c.ImGuiWindowFlags = c.ImGuiWindowFlags_None;
-
-    main_flags |= c.ImGuiWindowFlags_NoResize;
-    main_flags |= c.ImGuiWindowFlags_NoBackground;
-    main_flags |= c.ImGuiWindowFlags_NoMove;
-    main_flags |= c.ImGuiWindowFlags_NoScrollWithMouse;
-    main_flags |= c.ImGuiWindowFlags_NoBringToFrontOnFocus;
-    main_flags |= c.ImGuiWindowFlags_NoDecoration;
+    const viewport = c.igGetMainViewport();
+    // ImGui::SetNextWindowPos(viewport->GetWorkPos());
+    // ImGui::SetNextWindowSize(viewport->GetWorkSize());
+    // ImGui::SetNextWindowViewport(viewport->ID);
+    //
+    c.igSetNextWindowSize(viewport.*.WorkSize, c.ImGuiCond_None);
+    c.igSetNextWindowPos(.{ .x = 0, .y = 0 }, c.ImGuiCond_None, .{},);
+    c.igSetNextWindowViewport(viewport.*.ID);
 
     var main_is_open : bool = true;
-    defer c.igEnd();
-    if (!c.igBegin("###FULLSCREEN", @ptrCast(&main_is_open), main_flags))
+    if (
+        c.igBegin(
+            "###FULLSCREEN",
+            @ptrCast(&main_is_open),
+            c.ImGuiWindowFlags_NoDecoration 
+            | c.ImGuiWindowFlags_NoResize
+            | c.ImGuiWindowFlags_NoScrollWithMouse
+            | c.ImGuiWindowFlags_AlwaysAutoResize
+            | c.ImGuiWindowFlags_NoMove
+            ,
+        )
+    )
     {
-        return;
+        defer c.igEnd();
+
+        const style = c.igGetStyle();
+        style.*.WindowRounding = 0;
+        style.*.WindowPadding = .{ .x = 0, .y = 0 };
+
+        const text_header_size = 30;
+        // const text_size = c.ImVec2{
+        //     .x = viewport.*.WorkSize.x,
+        //     .y = text_header_size,
+        // };
+        // c.igSetNextWindowSize(text_size, c.ImGuiCond_None);
+
+        if (
+            c.igBeginChild_Str(
+                "Hello Child Plot",
+                // text_size,
+                viewport.*.WorkSize,
+                c.ImGuiChildFlags_None,
+                c.ImGuiWindowFlags_NoResize,
+            )
+        )
+        {
+            defer c.igEndChild();
+            // FPS/status line
+            c.igBulletText(
+                "Ok, pieces are working...sdfasdfasdfasdfasd filling the window so we can see it go all the way across blah blah blah\nAverage : %.3f ms/frame (%.1f fps)",
+                1000.0 / c.igGetIO().*.Framerate, c.igGetIO().*.Framerate,
+            );
+
+            var buf : [4048]u8 = undefined;
+
+            _ = try std.fmt.bufPrintZ(
+                &buf,
+                "viewport data:\n size: {any} \n worksize: {any} \n",
+                .{
+                    viewport.*.Size,
+                    viewport.*.WorkSize,
+                } 
+            );
+
+            c.igText(
+                "read data: %s",
+                &buf,
+            );
+        }
+        // c.igSetNextWindowPos(.{ .x = 0, .y = text_header_size }, 0, .{});
+
+        const next_window_size = c.ImVec2{
+            .x = viewport.*.WorkSize.x,
+            .y = viewport.*.WorkSize.y - text_header_size,
+        };
+
+        if (
+            c.igBeginChild_Str(
+                "Hello Child Plot",
+                next_window_size,
+                c.ImGuiChildFlags_None,
+                c.ImGuiWindowFlags_NoResize,
+            )
+        )
+        {
+            defer c.igEndChild();
+            if (c.ImPlot_BeginPlot(
+                    "Graph View",
+                    viewport.*.Size,
+                    c.ImPlotFlags_None,
+            )){
+                const xs= [_]f32{0, 1, 2, 3, 4};
+                const ys= [_]f32{0, 1, 2, 3, 6};
+
+                c.ImPlot_PlotLine_FloatPtrFloatPtr(
+                    "test plot",
+                    &xs,
+                    &ys,
+                    5,
+                    c.ImPlotLineFlags_None,
+                    0,
+                    @sizeOf(f32),
+                );
+                defer c.ImPlot_EndPlot();
+            }
+        }
     }
 
-    // FPS/status line
-    c.igBulletText(
-        "Ok, pieces are working...\nAverage : %.3f ms/frame (%.1f fps)",
-        1000.0 / c.igGetIO().*.Framerate, c.igGetIO().*.Framerate,
-    );
+
     // zgui.bulletText(
     //     "Average : {d:.3} ms/frame ({d:.1} fps)",
     //     .{ gfx_state.gctx.stats.average_cpu_time, gfx_state.gctx.stats.fps },
